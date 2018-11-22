ff414ca034408b17b47fc06fcf5747adcf67b70c
antoinevinc <44704438+antoinevinc@users.noreply.github.com>
2018-11-22 16:22:44 -0400

    Update exo_16.rb

--- exo_16.rb
+++ exo_16.rb
@@ -1,9 +1,8 @@
-puts "Veuillez entrer votre année de naissance :"
-year = gets.chomp.to_i
-age = 0
-while (year <= 2017)
-  x = 2017 - year
-  puts "Il y a " + x.to_s + " ans, tu avais " + age.to_s + " ans."
-  age = age + 1
+puts "Veuillez entrer votre age:"
+age = gets.chomp.to_i
+year = 0
+while (age >= 0)
+  puts "Il y a " + age.to_s + " ans, tu avais " + year.to_s + " ans."
+  age = age - 1
   year = year + 1;
 end
